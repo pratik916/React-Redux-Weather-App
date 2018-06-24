45148a3609a2a6eeb7c821c1911c4c6ff5ae6a3b
Pratik Soni <pratiksoni916@gmail.com>
2018-06-24 21:02:01 +0530

    Implemented weather API and added google map into it.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = '95750a2e92af8e88618bfc537ed3d32e';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
